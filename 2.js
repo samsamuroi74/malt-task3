ffb5d10a1fdac1553fe093506882dc6979595717
Sairam Sriram <samsamuroi74@gmail.com>
2021-07-16 12:32:55 +0530

    some changes

--- 2.js
+++ 2.js
@@ -4,8 +4,14 @@ p1.innerHTML = prompt("give here a value");
 
 $(function () {
     $("#divcol-1").slideUp();
+    $("#divcol-2").slideUp();
+    $("#divcol-3").slideUp();
+    $("#divcol-4").slideUp();
     $("#btn2-1").click(function () {
         $("#divcol-1").slideToggle("slow");
+        $("#divcol-2").slideToggle("slow");
+        $("#divcol-3").slideToggle("slow");
+        $("#divcol-4").slideToggle("slow");
        
     })
     
